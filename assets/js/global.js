2892af17797f944fad0019b1210ceaa544ff61d8
Tasha (Mayes) Dular <telizabethm@gmail.com>
2017-04-20 11:36:08 -0400

    If current floor
    if you are the current floor

--- assets/js/global.js
+++ assets/js/global.js
@@ -9,17 +9,27 @@ $(document).ready(function(){
     var OpenDoor = "Opened";
     var CloseDoor = "Closed";
 
+
     //Get Floor once selected
     $("#SelectFloor").on("change", function() {
-    	    var SelectFloor = $('#SelectFloor option:selected').val();
+    	   var SelectFloor = $('#SelectFloor option:selected').val();
+    	   var CurrentFloor = $('#CurrentFloor').val();
+    	   var Elevator = $('#Elevator').val(SelectFloor);
 
-    	   $('#Elevator').val(SelectFloor);
+    	    //If Current Floor
+		    if (CurrentFloor == GroundFloor ) {
+		    	$("p").text(OpenDoor + " : " + "You are already on the ground floor");
+		    }
+		    else {
+		    	$("p").text(CloseDoor);
+		    }
 	});
 
     //Show Floor of Elevator
     $('#submit').click(function () {
-    	    var SelectFloor = $('#SelectFloor option:selected').val();
-
+    	  var SelectFloor = $('#SelectFloor option:selected').val();
     	 $('#Elevator').val(SelectFloor);
     });
+
+
 });
