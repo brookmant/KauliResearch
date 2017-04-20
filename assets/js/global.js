b6e2801083d3a8c4f2516f7f57ed4021dbd510f7
Tasha (Mayes) Dular <telizabethm@gmail.com>
2017-04-20 10:49:32 -0400

    Updates
    setup project

---
+++ assets/js/global.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+    
+    //Floors
+    var GroundFloor = 1;
+    var TopFloor = 7;
+    var ElevatorMove = "Moving";
+
+    //Evavator Actions
+    var OpenDoor = "Opened";
+    var CloseDoor = "Closed";
+
+    $('#Elevator').text(GroundFloor);
+
+});
